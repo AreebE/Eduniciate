8e764442fc14ba1d2edaf304d0d241cbd4312770
Sushi12 <75046551+Sushi12@users.noreply.github.com>
2022-08-15 23:19:08 -0700

    Update loginscreen.dart

--- lib/signUpScreen/loginscreen.dart
+++ lib/signUpScreen/loginscreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:edunciate/signUpScreen/signupscreen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 
 class LogInScreen extends StatelessWidget {
@@ -16,44 +18,133 @@ class LogInScreen extends StatelessWidget {
           children: <Widget>[
             Text('Edunicate', style: TextStyle(fontSize:24, fontFamily: 'Josefin Sans')),
             Spacer(),
-            Text( 'Log In', style: TextStyle(fontSize: 24, fontFamily: 'Josefin Sans'))
+            Text( 'Login', style: TextStyle(fontSize: 24, fontFamily: 'Josefin Sans'))
             ] 
           ),
         backgroundColor: Color(0xffF9461E1),
         ),
-        body: MyHomePage(),
+        body: MyCustomForm(),
       )
     );
   }
 }
 
-class MyHomePage extends StatelessWidget{
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  State<MyCustomForm> createState() => _MyCustomFormState();
+}
+
+class _MyCustomFormState extends State<MyCustomForm> {
+  final emailLogin = TextEditingController();
+  final passwordLogin = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    emailLogin.dispose();
+    passwordLogin.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
     body: Column(
       children: <Widget>[
         EmailPasswordBox('Email'),
-
-        EmailPasswordBox('Password'),
-        
-          SizedBox(height: 30),
-          SizedBox (
+        Container(
+          decoration:BoxDecoration(border: Border.all()),
           height: 30,
-          width: 90,            
+          width: MediaQuery. of(context). size. width *.80,
+          padding: EdgeInsets.symmetric(horizontal:2),
+          child: TextField(textAlignVertical: TextAlignVertical.center, controller: emailLogin)
+        ),
+      
+        EmailPasswordBox('Password'),
+        Container(
+          decoration:BoxDecoration(border: Border.all()),
+          height: 30,
+          width: MediaQuery. of(context). size. width *.80,
+          padding: EdgeInsets.symmetric(horizontal:2),
+          child: TextField(textAlignVertical: TextAlignVertical.center, controller: passwordLogin)
+        ),
+        SizedBox(height: 30),
+         
+        SizedBox (
+          height: 30,
+          width: 90,
           child: ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              LoginHelp(context, emailLogin.text, passwordLogin.text);
+            },
             style: ElevatedButton.styleFrom (primary: Color(0xffF5A2E9C), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(9.0))),
-            child: Text('Log In', style: TextStyle( fontSize: 15, fontFamily: 'Lato', color: Colors.white),
+            child: Text('Log in', style: TextStyle( fontSize: 15, fontFamily: 'Lato', color: Colors.white),
             ),
           ),
+        ),
+        SizedBox(height:20),
+          SizedBox(
+            width: 200,
+            height: 30,
+            child: ElevatedButton(
+              onPressed: () {
+                forgotPasswordDialog(context);
+              },
+              style: ElevatedButton.styleFrom(primary: Color(0xffF5A2E9C), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(9.0))),
+              child: Text('Forgot Password?', style: TextStyle( fontSize: 15, fontFamily: 'Lato', color: Colors.white))
+            )
           ),
-          AlternateButtons('OR', 'LOG IN WITH GOOGLE'),
+          AlternateButtons('OR', 'SIGN UP WITH GOOGLE'),
           AlternateButtons('DON\'T HAVE AN ACCOUNT?', 'CONTINUE TO SIGN UP PAGE'),
         ],
-      ),
+    ),
     );
   }
+}
+
+Future<bool> passwordReset(BuildContext context, String email) async {
+  try {
+    await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+  } catch (e) {
+    showAlertDialog(context, 'PROBLEM WITH EMAIL', 'Try to make sure your email is typed correctly!');
+    return false;
+  }
+  return true;
+}
+
+forgotPasswordDialog(BuildContext context) {
+final emailController = TextEditingController();
+
+
+  Widget emailGetter = Container(
+    decoration:BoxDecoration(border: Border.all()),
+    height: 30,
+    width: 250,
+    padding: EdgeInsets.symmetric(horizontal:2),
+    child: TextField(textAlignVertical: TextAlignVertical.center, controller: emailController)
+  );
+
+  Widget submit = TextButton(
+    child: Text("Submit"),
+    onPressed:  () {
+     passwordReset(context, emailController.text);
+    },
+  );
+
+  AlertDialog alert = AlertDialog(
+    title: Text('Type in you email and we will send a password reset link to your email.'),
+    actions: [ emailGetter, submit]
+  );
+
+  // show the dialog
+   showDialog (
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    }
+  );
 }
 
 class EmailPasswordBox extends StatelessWidget {
@@ -69,13 +160,6 @@ class EmailPasswordBox extends StatelessWidget {
         SizedBox(
           width:  MediaQuery. of(context). size. width *.80,
           child:Text(aboveText, style: TextStyle(fontSize: 15, fontFamily: 'Lato'), textAlign: TextAlign.left,)
-        ),
-        Container(
-          decoration:BoxDecoration(border: Border.all()),
-          height: 30,
-          width: MediaQuery. of(context). size. width *.80,
-          padding: EdgeInsets.symmetric(horizontal:2),
-          child: TextField(textAlignVertical: TextAlignVertical.center)
         ),
         ],
       )
@@ -105,6 +189,9 @@ class AlternateButtons extends StatelessWidget {
                 if (buttonText == 'CONTINUE TO SIGN UP PAGE') {
                   Navigator.push(context, MaterialPageRoute(builder: (context) => const SignUpScreen()));
                 }
+                else if(buttonText == 'SIGN IN WITH GOOGLE') {
+                  signInWithGoogle();
+                }
               },
               style: ElevatedButton.styleFrom(primary: Color(0xffF9461E1), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(9.0))),
                 child:Text(buttonText, style: TextStyle( fontSize: 15, fontFamily: 'Lato', color: Colors.white), textAlign: TextAlign.left)
@@ -116,4 +203,18 @@ class AlternateButtons extends StatelessWidget {
   }
 }
 
+Future<bool> LoginHelp(BuildContext context, String emailAddress, String password) async {
+  try {
+    final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: emailAddress,
+      password: password
+    );
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'user-not-found') {
+      showAlertDialog(context, 'USER NOT FOUND', 'Please check that your email address is typed correctly');
+    } else if (e.code == 'wrong-password') {
+    showAlertDialog(context, 'PASSWORD WRONG', 'Please check that your password is typed corrrectly');    }
+  }
+    return true;
+}
 
